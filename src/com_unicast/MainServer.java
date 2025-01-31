f5218ace0b76dc774bced5b4c4c118399be8a20a
samuelepierotti <samy.pierotti@gmail.com>
2025-01-31 11:15:54 +0100

    Update MainServer.java

--- src/com_unicast/MainServer.java
+++ src/com_unicast/MainServer.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MainServer {
 
     public static void main(String[] args) {
         Server s= new Server(2000);
