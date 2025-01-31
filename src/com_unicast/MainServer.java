a3ea4f66389db7a0a42195710df6034f5f29ee27
samuelepierotti <samy.pierotti@gmail.com>
2025-01-31 11:12:26 +0100

    Modifica classe MainServer.java

--- src/com_unicast/MainServer.java
+++ src/com_unicast/MainServer.java
@@ -1,21 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package com_unicast;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
 
-/**
- *
- * @author studente
- */
-public class MainServer {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Server s = new Server(1905);
-        s.attend();
+        Server s= new Server(2000);
+        s.attendi();
     }
-    
+
 }
